ab899619021b0d8996cae09a1733bae3c369b5c7
Ramees Ahmed <ar.ramees.ar@gmail.com>
2024-02-07 13:19:40 +0530

    selenium nodeJs sample setup with lambda key integration

--- index.js
+++ index.js
@@ -10,13 +10,16 @@
     Execute NodeJS Automation Tests on LambdaTest Distributed Selenium Grid 
 */
 
-const webdriver = require('selenium-webdriver');
+import * as dotenv from "dotenv";
+import { Builder, By } from 'selenium-webdriver';
+dotenv.config();
 
 // username: Username can be found at automation dashboard
 const USERNAME = process.env.LT_USERNAME;
 
 // AccessKey:  AccessKey can be generated from automation dashboard or profile section
 const KEY = process.env.LT_ACCESS_KEY;
+console.log("Key.....",USERNAME, KEY)
 
 // gridUrl: gridUrl can be found at automation dashboard
 const GRID_HOST = 'hub.lambdatest.com/wd/hub';
@@ -41,7 +44,7 @@ async function searchTextOnGoogle() {
     const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
 
     // Setup and build selenium driver object
-    const driver = new webdriver.Builder()
+    const driver = new Builder()
         .usingServer(gridUrl)
         .withCapabilities(capabilities)
         .build();
@@ -49,14 +52,18 @@ async function searchTextOnGoogle() {
     try {
         // Navigate to a URL, click on the first and second list items and add a new one in the list.
         await driver.get('https://lambdatest.github.io/sample-todo-app/');
-        await driver.findElement(webdriver.By.name('li1')).click();
+        await driver.findElement(By.name('li1')).click();
         console.log("Successfully clicked first list item.");
-        await driver.findElement(webdriver.By.name('li2')).click();
+        await driver.findElement(By.name('li2')).click();
         console.log("Successfully clicked second list item.");
+        await driver.findElement(By.name('li5')).click();
+        console.log("Successfully clicked fifth list item.");
 
-        await driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n');
-        await driver.findElement(webdriver.By.id('addbutton')).click();
+        await driver.findElement(By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n');
+        await driver.findElement(By.id('addbutton')).click();
         console.log("Successfully added a new task.");
+        await driver.findElement(By.name('li6')).click();
+        console.log("Successfully clicked sixth list item.");
     } catch (err) {
         console.log("test failed with reason " + err);
         await driver.executeScript('lambda-status=failed');
